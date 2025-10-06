38785c2198b363a9177a91618ee3ae31bca498cc
ISAAC_MACHADO_PACHECO_RAMOS <22502696@ALFAF23.FLORESTA>
2025-10-06 10:43:56 -0300

    teste

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let botao = document.getElementById('modos');
 botao.addEventListener('click', () => {
   document.body.classList.toggle('modoclaro');
 });
+
+let enviar =document.getElementById('enviar')
+
+enviar.addEventListener('click',() => {
+  enviar.style.backgroundColor = '#ffffff';
+})
